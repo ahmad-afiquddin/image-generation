82c882f645586acb952a04435aaa0bb155416ad9
intan farhana <intanfarhana@intans-MacBook-Air.local>
2025-04-19 12:04:39 +0800

    fix: missing module imports

--- src/common/storage/storage.module.ts
+++ src/common/storage/storage.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { MinioService } from '@common/storage/minio.service';
 import { StorageService } from '@common/storage/storage.service';
 
 @Module({
+  imports: [ConfigModule],
   providers: [StorageService, MinioService],
-  exports: [StorageService],
+  exports: [StorageService, MinioService],
 })
 export class StorageModule {}
